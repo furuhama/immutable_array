7a87fb9a4def50175afb6b9adad52d6afb99d32e
furuhama <furuhama@users.noreply.github.com>
2018-02-04 10:19:31 +0900

    Make prototype(add recursive_freeze)

--- lib/immutable_array.rb
+++ lib/immutable_array.rb
@@ -1,7 +1,41 @@
 require "immutable_array/version"
 
-module ImmutableArray
-  def self.hoge
-    puts 'hoge!'
+class ImmutableArray < Array
+  def self.new(size=0, val=nil)
+    if size.class == Array
+      puts 'array has come!'
+      size.recursive_freeze
+    else
+      puts 'size has come!'
+      Array.new(size, val).recursive_freeze
+    end
+  end
+
+  # freeze elements recursively
+  def recursive_freeze
+    self.each { |x|
+      if x.class == Array
+        x.recursive_freeze
+      end
+
+      x.freeze
+    }
+    self.freeze
+  end
+
+  # prototype
+  #
+  # Usage:
+  # [1, 3, 6, 7] << :freeze
+  #
+  # all elements and itself is frozen
+  #
+  # and override '<<' operator
+  # not to add element destructively
+  def <<(arg)
+    arg = arg.to_sym if arg.class == String
+
+    self.map(&arg)
+    self.send(arg)
   end
 end
