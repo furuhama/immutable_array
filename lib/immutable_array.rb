23b6cd2829ee145a77238552f7d57cff8221dba7
furuhama <furuhama@users.noreply.github.com>
2018-02-04 10:21:10 +0900

    Put them into module

--- lib/immutable_array.rb
+++ lib/immutable_array.rb
@@ -1,41 +1,43 @@
 require "immutable_array/version"
 
-class ImmutableArray < Array
-  def self.new(size=0, val=nil)
-    if size.class == Array
-      puts 'array has come!'
-      size.recursive_freeze
-    else
-      puts 'size has come!'
-      Array.new(size, val).recursive_freeze
-    end
-  end
-
-  # freeze elements recursively
-  def recursive_freeze
-    self.each { |x|
-      if x.class == Array
-        x.recursive_freeze
+module ImmutableArray
+  class ImmutableArray < Array
+    def self.new(size=0, val=nil)
+      if size.class == Array
+        puts 'array has come!'
+        size.recursive_freeze
+      else
+        puts 'size has come!'
+        Array.new(size, val).recursive_freeze
       end
+    end
 
-      x.freeze
-    }
-    self.freeze
-  end
+    # freeze elements recursively
+    def recursive_freeze
+      self.each { |x|
+        if x.class == Array
+          x.recursive_freeze
+        end
 
-  # prototype
-  #
-  # Usage:
-  # [1, 3, 6, 7] << :freeze
-  #
-  # all elements and itself is frozen
-  #
-  # and override '<<' operator
-  # not to add element destructively
-  def <<(arg)
-    arg = arg.to_sym if arg.class == String
+        x.freeze
+      }
+      self.freeze
+    end
 
-    self.map(&arg)
-    self.send(arg)
+    # prototype
+    #
+    # Usage:
+    # [1, 3, 6, 7] << :freeze
+    #
+    # all elements and itself is frozen
+    #
+    # and override '<<' operator
+    # not to add element destructively
+    def <<(arg)
+      arg = arg.to_sym if arg.class == String
+
+      self.map(&arg)
+      self.send(arg)
+    end
   end
 end
