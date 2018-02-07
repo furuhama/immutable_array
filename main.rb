71df6e580973edf828a13ed8049652f4d3af97f0
furuhama <furuhama@users.noreply.github.com>
2018-02-07 16:40:41 +0900

    Edit main.rb

--- main.rb
+++ main.rb
@@ -8,8 +8,4 @@ if __FILE__ == $0
   arr = [10, 'hoge', :haskell, [1, 'fuga', :nyaa]] << :freeze
 
   p arr.recursive_bool_check(:frozen?)
-
-  arr2 = [10, 20, 30] << :freeze
-  arr2 << :p
-  p arr2
 end
